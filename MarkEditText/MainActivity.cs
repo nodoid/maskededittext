4a1bcea17b719ab2828befb17e09d1274f45dee3
Paul Johnson <paul@all-the-johnsons.co.uk>
2015-05-25 11:20:42 +0100

    fixes

--- MarkEditText/MainActivity.cs
+++ MarkEditText/MainActivity.cs
@@ -8,13 +8,14 @@ namespace MaskedEditText
     [Activity(Label = "MaskedEditText", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
+
+            var maskedText = FindViewById<MaskedEditText>(Resource.Id.phoneEdit);
+            maskedText.Mask = "+@@:@@@@:@@@@@@";
         }
     }
 }
